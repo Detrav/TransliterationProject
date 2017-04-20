92df0f040d0fe0391dd5d86e392fda76d9da4a50
Witaly Ezepchuk <witaly@fast-report.com>
2017-04-20 15:25:15 +0300

    + Init

---
+++ TrProject/TrProject.ProjectType/VsPackage.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+namespace TransliterationProject
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// This package is required if you want to define adds custom commands (ctmenu)
+    /// or localized resources for the strings that appear in the New Project and Open Project dialogs.
+    /// Creating project extensions or project types does not actually require a VSPackage.
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Description("A custom project type based on CPS")]
+    [Guid(VsPackage.PackageGuid)]
+    public sealed class VsPackage : Package
+    {
+        /// <summary>
+        /// The GUID for this package.
+        /// </summary>
+        public const string PackageGuid = "cc19d215-a208-4930-b6be-4296eb50524e";
+
+        /// <summary>
+        /// The GUID for this project type.  It is unique with the project file extension and
+        /// appears under the VS registry hive's Projects key.
+        /// </summary>
+        public const string ProjectTypeGuid = "8d6666db-c391-4591-bbdd-f82d2858f2cb";
+
+        /// <summary>
+        /// The file extension of this project type.  No preceding period.
+        /// </summary>
+        public const string ProjectExtension = "trproj";
+
+        /// <summary>
+        /// The default namespace this project compiles with, so that manifest
+        /// resource names can be calculated for embedded resources.
+        /// </summary>
+        internal const string DefaultNamespace = "TransliterationProject";
+    }
+}
